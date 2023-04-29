f91c20a7f7445d740035ac6b50f445be9ec3595e
Lu Niu <lniu6305@headnode.physics.usyd.edu.au>
2023-04-30 07:10:16 +1000

    Update

--- VASP/Bulk_fcc_Si_bandstructure/vasp_job.sh
+++ VASP/Bulk_fcc_Si_bandstructure/vasp_job.sh
@@ -7,30 +7,13 @@
 #PBS -m a
 #PBS -M luke.niu@sydney.edu.au
 
+cp ../Bulk_fcc_Si_DOS/CHGCAR
+
 cd "$PBS_O_WORKDIR"
 
 module purge
 module load pbspro-intelmpi 
 module load compiler-rt mpi mkl
 set VASP=~/VASP544/bin/vasp_std
-set BIN=~/VASP544/bin/vasp_std
 
-rm -f WAVECAR SUMMARY.fcc
-
-foreach i (3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9 5.0)
-cat > POSCAR << END
-fcc:
-    $i
-    0.5 0.5 0.0
-    0.0 0.5 0.5
-    0.5 0.0 0.5
-    1
-    cartesian
-    0 0 0
-END
-echo "a= $i"
-mpirun -np 12 $BIN
-set E=`awk '/F=/ {print $0}' OSZICAR` ; echo $i $E  >> SUMMARY.fcc
-end
-
-cat SUMMARY.fcc
+mpirun -np 12 $VASP > vasp.out
