b08d822cb9204b5b521a5b48aeb800ea12be33e8
Photonico <LukeNiu@Outlook.com>
2023-06-14 14:14:25 +1000

    Update

---
+++ VASP_g-BC3_20230526/3_Bandstructure_gamma/vasp_job.sh
@@ -0,0 +1,21 @@
+#!/bin/csh
+#PBS -N BC3_bandstructure_gamma
+#PBS -q defaultQ
+#PBS -j oe
+#PBS -l select=1:ncpus=12:mpiprocs=12:mem=30GB
+#PBS -l walltime=48:00:00
+#PBS -m a
+#PBS -M luke.niu@sydney.edu.au
+
+# Calculating bandstructure need initial density of charges
+# cp ../<>/CHGCAR
+
+cd "$PBS_O_WORKDIR"
+
+module purge
+module load pbspro-intelmpi 
+module load compiler-rt mpi mkl
+set VASP=~/VASP544/bin/vasp_std
+set BIN=~/VASP544/bin/vasp_std
+
+mpirun -np 12 $VASP > vasp.out
