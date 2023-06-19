c6c0ac927f8cdc06ebcfac82de2bf5ef4960d9f7
Photonico <LukeNiu@outlook.com>
2023-06-19 19:14:54 +1000

    Update

--- VASP_g-BC3_20230526/5_Phonon_dispersion_gamma/script_phonopy.py
+++ VASP_g-BC3_20230526/5_Phonon_dispersion_gamma/script_phonopy.py
@@ -2,6 +2,6 @@
 
 import subprocess
 
-BASH_COMMAND = "phonopy -d --dim 8 8 1 --pa auto -c POSCAR-unitcell"
+BASH_COMMAND = "phonopy -d --dim 4 4 1 --pa auto -c POSCAR-unitcell"
 
 subprocess.run(BASH_COMMAND, shell=True, check=True)
