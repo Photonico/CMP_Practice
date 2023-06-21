c4f7fb150ff4b836f898c50f2feb02d243771f6d
Photonico <LukeNiu@Outlook.com>
2023-06-21 11:47:57 +1000

    Update

--- VASP_g-BC3_20230526/5_Phonon_dispersion_gamma/script_phonopy.py
+++
@@ -1,7 +0,0 @@
-""" Script for Phonon Dispersion """
-
-import subprocess
-
-BASH_COMMAND = "phonopy -d --dim 4 4 1 --pa auto -c POSCAR-unitcell"
-
-subprocess.run(BASH_COMMAND, shell=True, check=True)
